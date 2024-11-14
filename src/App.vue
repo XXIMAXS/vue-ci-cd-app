38ca3e5ce35102705e152d6f6becbb00a71b046f
XXIMAXS <maksbaranjyk2005@gmail.com>
2024-11-14 11:41:44 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Калькулятор</h1>
+    <input v-model.number="a" placeholder="Введіть число A" />
+    <input v-model.number="b" placeholder="Введіть число B" />
+    <button @click="calculateSum">Обчислити суму</button>
+    <p>Сума: {{ sum }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      sum: 0,
+    };
+  },
+  methods: {
+    calculateSum() {
+      this.sum = this.a + this.b;
+    },
+  },
+};
+</script>
